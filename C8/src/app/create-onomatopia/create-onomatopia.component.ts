9550cd10c523946b9db3a22b6b72aaff4f60172c
LAURA_MAYA DD <mayadray1@gmail.com>
2023-11-06 16:33:21 +0100

    install

---
+++ C8/src/app/create-onomatopia/create-onomatopia.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-onomatopia',
+  templateUrl: './create-onomatopia.component.html',
+  styleUrls: ['./create-onomatopia.component.scss']
+})
+export class CreateOnomatopiaComponent {
+
+}
